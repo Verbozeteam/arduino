50b3ca75dce5c581a0eedaf85057793800895beb
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2017-09-28 11:03:40 +0300

    Rewrote sync protocol

--- build/lib/VCommunication/VCommunication.cpp
+++ build/lib/VCommunication/VCommunication.cpp
@@ -5,15 +5,15 @@
 
 extern uchar on_command(uchar msg_type, uchar msg_len, char* command_buffer);
 
-char GOOD_SYNC_SEQUENCE[8] = {(char)254, (char)6, (char)253, (char)11, (char)76, (char)250, (char)250, (char)255};
+char FULL_SYNC_SEQUENCE[8] = {(char)254, (char)6, (char)253, (char)11, (char)76, (char)250, (char)250, (char)255};
 char SYNC_SEQUENCE[8] = {(char)254, (char)6, (char)252, (char)11, (char)76, (char)250, (char)250, (char)255};
 const int SYNC_SEQUENCE_LEN = sizeof(SYNC_SEQUENCE) / sizeof(char);
 char command_buffer[MAX_COMMAND_SIZE];
 
 unsigned long sync_send_timer = 0;
 unsigned long sync_send_period = 1000; // 1 second
-int wait_for_sync = 1;
-int fully_synced = 0;
+int half_sync = 0;
+int full_sync = 0;
 
 HardwareSerial* SerialRef = NULL;
 
@@ -22,7 +22,17 @@ void serial_init(HardwareSerial* serial) {
     SerialRef->begin(9600);
 }
 
-uchar serial_is_synced() {
+uchar serial_is_synced(int set_to = -1) {
+    if (set_to != -1) {
+        half_sync = set_to;
+        full_sync = set_to;
+        if (set_to == 0) {
+            sync_send_timer = 0;
+            sync_send_period = 1000;
+        } else {
+            sync_send_period = 10000;
+        }
+    }
     return fully_synced == 1;
 }
 
@@ -36,18 +46,16 @@ void serial_update(unsigned long cur_time) {
     if (cur_time >= sync_send_timer) {
         sync_send_timer = cur_time;
         sync_send_timer += sync_send_period;
-        if (wait_for_sync) {
+        if (full_sync) {
             for (int i = 0; i < 8; i++)
-                SerialRef->write(SYNC_SEQUENCE[i]);
+                SerialRef->write(FULL_SYNC_SEQUENCE[i]);
         } else {
             for (int i = 0; i < 8; i++)
-                SerialRef->write(GOOD_SYNC_SEQUENCE[i]);
+                SerialRef->write(SYNC_SEQUENCE[i]);
         }
     }
 
-    if (wait_for_sync) {
-        fully_synced = 0;
-        sync_send_period = 1000;
+    if (!full_sync) {
         // look for a sync sequence
         int rb_size = read_buffer_size();
         if (rb_size >= SYNC_SEQUENCE_LEN) {
@@ -56,7 +64,7 @@ void serial_update(unsigned long cur_time) {
             for (sync_start = 0; sync_start + SYNC_SEQUENCE_LEN < rb_size; sync_start++) {
                 int is_sync_sequence_start = 1;
                 for (int i = 0; i < SYNC_SEQUENCE_LEN; i++) {
-                    if (read_buffer_at(sync_start+i) != SYNC_SEQUENCE[i] && read_buffer_at(sync_start+i) != GOOD_SYNC_SEQUENCE[i]) {
+                    if (read_buffer_at(sync_start+i) != SYNC_SEQUENCE[i] && read_buffer_at(sync_start+i) != FULL_SYNC_SEQUENCE[i]) {
                         is_sync_sequence_start = 0;
                         break;
                     }
@@ -68,9 +76,12 @@ void serial_update(unsigned long cur_time) {
             }
 
             if (found_sync) {
-                wait_for_sync = 0;
-                sync_send_period = 10000;
+                int found_full = read_buffer_at(sync_start+2) == FULL_SYNC_SEQUENCE[2];
                 read_buffer_consume(sync_start+SYNC_SEQUENCE_LEN);
+                if (found_full)
+                    serial_is_synced(1);
+                else
+                    half_sync = 1;
             } else {
                 int truncate_size = read_buffer_size() - SYNC_SEQUENCE_LEN;
                 read_buffer_consume(truncate_size);
@@ -79,7 +90,7 @@ void serial_update(unsigned long cur_time) {
     }
 
     int rb_size = read_buffer_size();
-    while (!wait_for_sync && rb_size > 2) {
+    while (full_sync && rb_size > 2) {
         char msg_type = read_buffer_at(0);
         char msg_len = read_buffer_at(1);
         if (rb_size >= 2 + msg_len) {
@@ -91,24 +102,21 @@ void serial_update(unsigned long cur_time) {
             if (msg_type == SYNC_SEQUENCE[0]) {
                 int is_valid = msg_len == SYNC_SEQUENCE[1] ? 1 : 0;
                 for (int i = 2; i < SYNC_SEQUENCE_LEN && is_valid; i++)
-                    if (command_buffer[i-2] != SYNC_SEQUENCE[i] && command_buffer[i-2] != GOOD_SYNC_SEQUENCE[i])
+                    if (command_buffer[i-2] != FULL_SYNC_SEQUENCE[i])
                         is_valid = 0;
                 if (!is_valid)
-                    wait_for_sync = 1;
-                else if (command_buffer[0] == GOOD_SYNC_SEQUENCE[2])
-                    fully_synced = 1;
+                    serial_is_synced(0);
             } else {
-                if (!on_command(msg_type, msg_len, &command_buffer[0])){
-                    //wait_for_sync = 1;
-                }
+                if (!on_command(msg_type, msg_len, &command_buffer[0]))
+                    serial_is_synced(0);
             }
         } else
             break;
         rb_size = read_buffer_size();
     }
 
-    digitalWrite(45, wait_for_sync ? HIGH : LOW);
-    digitalWrite(44, fully_synced ? HIGH : LOW);
+    digitalWrite(45, half_sync ? HIGH : LOW);
+    digitalWrite(44, full_sync ? HIGH : LOW);
 }
 
 void send_serial_command(uchar type, uchar len, char* cmd) {
