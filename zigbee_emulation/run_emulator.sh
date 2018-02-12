c18d8a7c5dd5e89583a8bae5b137ba148099190b
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-02-12 16:04:10 +0300

    Removed binary

---
+++ zigbee_emulation/run_emulator.sh
@@ -0,0 +1,1 @@
+python ../../shammam/shammam.py -n mega2560 -p ../../shammam/arduino_protocol.proto -i ../build/lib/V*/ ../emulation/inc/ -s ../emulation/src/TimerOne.cpp ../build/lib/V*/*.cpp main.cpp
